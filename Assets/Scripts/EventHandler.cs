a689674f7a6b7d9ac2cae8c5cc5dee84cce99717
TheIronFortress <K_Brooksby@hotmail.com>
2021-09-21 09:41:18 -0600

    Initial Commit
    1. Copied PP to new repo to circumvent old broken repo

---
+++ Assets/Scripts/EventHandler.cs
@@ -0,0 +1,188 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour
+{
+    // Types of upgrades
+    public enum Upgrades {
+        Horsepower,
+        Flexibility,
+        Torque,
+        Time
+    }
+
+    // Fields
+    public int currency;
+    public float runTimer;
+    public bool inRun;
+    [SerializeField] LevelLoader loader;
+    [SerializeField] GameObject shopUI;
+    [SerializeField] GameObject timeUI;
+    [SerializeField] Vector2 spawn;
+    [SerializeField] Animator anim;
+    [SerializeField] List<AudioSource> audioSources;
+
+    // Event Handler Hiddens
+    int runDistance;
+    int timeExtension;
+    int maxLevel;
+    PlayerController player;
+    public Dictionary<Upgrades, int> upgradeLevels = new Dictionary<Upgrades, int>();
+
+    // Upgrades
+    Stack<int> horsepowers = new Stack<int>(new int[] {380, 330, 280});
+    Stack<int> flexibilities = new Stack<int>(new int[] {30, 25, 20});
+    Stack<int> torques = new Stack<int>(new int[] {25, 20, 15});
+    Stack<int> timeExtensions = new Stack<int>(new int[] {25, 20, 15});
+
+    // Sets up all initial values
+    private void Start()
+    {
+        // Dictionary Setup
+        upgradeLevels.Add(Upgrades.Horsepower, 0);
+        upgradeLevels.Add(Upgrades.Flexibility, 0);
+        upgradeLevels.Add(Upgrades.Torque, 0);
+        upgradeLevels.Add(Upgrades.Time, 0);
+
+        // Other Hiddens
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        runDistance = 0;
+        timeExtension = 10;
+        maxLevel = 3;
+        inRun = false;
+
+        // Start shop music
+        audioSources[0].Play();
+    }
+
+    // Checks for status updates once per frame
+    private void Update()
+    {
+        // Increment Timers
+        runTimer -= Time.deltaTime;
+        runDistance = (int)(new Vector2(player.transform.position.x, player.transform.position.y).magnitude);
+
+        // Check fail condition
+        if (runTimer < 0 && inRun)
+            EngageFailState();
+    }
+
+    // Tries to add currency
+    public bool AddCurrency(int amount) 
+    {
+        if (amount > 0)
+        {
+            currency += amount;
+            return true;
+        }
+        return false;
+    }
+
+    // Tries to remove currency
+    public bool RemoveCurrency(int amount)
+    {
+        int difference = currency - amount;
+        if (difference > 0)
+        {
+            currency = difference;
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Tries to buy an upgrade for the corresponding characteristic
+    /// </summary>
+    /// <param name="upgrade">The upgrade being bought</param>
+    /// <returns>
+    /// True if the purchase was successful
+    /// false if it was not
+    /// </returns>
+    public bool Buy(Upgrades upgrade)
+    { 
+        upgradeLevels.TryGetValue(upgrade, out int upgradeLevel);
+        if (upgradeLevel < maxLevel && RemoveCurrency(GetPrice(upgradeLevel)))
+        {
+            anim.SetTrigger("SuccesfulPurchase");
+            Upgrade(upgrade);
+            return true;
+        }
+        return false;
+        //TODO: Shopkeep animation
+    }
+
+    // Gets the price for the corresponding level
+    int GetPrice(int upgradeLevel)
+    {
+        return (upgradeLevel + 1) * 100;
+    }
+
+    // Upgrades the corresponding characteristic
+    void Upgrade(Upgrades upgrade)
+    {
+        if (upgradeLevels.TryGetValue(upgrade, out int level)){
+            upgradeLevels[upgrade] = level + 1;
+            switch (upgrade)
+            {
+                case Upgrades.Horsepower:
+                    if (horsepowers.Count > 0)
+                        Debug.Log(horsepowers.Peek());
+                        player.horsepower = horsepowers.Pop();
+                    break;
+                case Upgrades.Flexibility:
+                    if (flexibilities.Count > 0)
+                        player.flexibility = flexibilities.Pop();
+                    break;
+                case Upgrades.Torque:
+                    if (torques.Count > 0)
+                        player.torque = torques.Pop();
+                    break;
+                case Upgrades.Time:
+                    if (timeExtensions.Count > 0)
+                        timeExtension = timeExtensions.Pop();
+                    break;
+            }
+        }
+        
+    }
+
+    public void ResetRun() 
+    {
+        StartCoroutine(ReturnToShop());
+    }
+
+    public void StartRun()
+    {
+        audioSources[0].Stop();
+        audioSources[1].Play();
+        shopUI.SetActive(false);
+        timeUI.SetActive(true);
+        runTimer = 30 + timeExtension; // magic number
+        inRun = true;
+    }
+
+    public void EngageFailState()
+    {
+        inRun = false;
+        AddCurrency(runDistance);
+        ResetRun();
+    }
+
+    IEnumerator ReturnToShop() 
+    {
+        loader.ReplayTransition();
+        timeUI.SetActive(false);
+        audioSources[1].Stop();
+        audioSources[0].Play();
+        yield return new WaitForSeconds(1.5f);
+        player.transform.position = spawn;
+        shopUI.SetActive(true);
+    }
+
+    public void ExtendTime() 
+    {
+        Debug.Log("Extend");
+        runTimer += timeExtension;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Upgradeables
+    public float horsepower; // Formerly jumpPower
+    public float flexibility; // Formerly maxRotationAngle
+    public float torque; // Formerly rotationSpeed
+
+    // Player Fields
+    [SerializeField] bool movementEnabled;
+    [SerializeField] float jumpDelay; // in seconds
+    [SerializeField] Transform top;
+    [SerializeField] Transform bottom;
+    [SerializeField] float centerOfMass;
+
+    // Player Hiddens
+    bool jumpIntent;
+    float moveDirection;
+    float jumpTimer;
+    Rigidbody2D rb;
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initializes all Player Hiddens
+        movementEnabled = true;
+        jumpIntent = false;
+        rb = GetComponent<Rigidbody2D>();
+        rb.centerOfMass = Vector2.up * centerOfMass;
+        jumpTimer = jumpDelay;
+        moveDirection = 0;
+        audio = GetComponent<AudioSource>();
+    }
+    
+    // Update is called on every frame.
+    private void Update()
+    {
+        // Increment timers
+        jumpTimer += Time.deltaTime;
+
+        // Handles movement input
+        moveDirection = Input.GetAxis("Horizontal");
+
+        // Determines if the player wants to use a jump
+        if (Input.GetKey("space"))
+        {
+            jumpIntent = true;
+        }
+    }
+
+    // FixedUpdate is called regularly. This is for executing movement controls.
+    private void FixedUpdate()
+    {
+        // Checks to see if the player is allowed to move
+        if (movementEnabled)
+        {
+            Move();
+            Jump();
+        }
+    }
+
+    // Handles movement
+    private void Move() 
+    {
+        // Uses the intended movement direction to determine the angle of the next bend.
+        float targetRotation = bottom.localRotation.eulerAngles.z + torque * moveDirection;
+
+        // Bounds the bending to within maxRotation in either direction.
+        if (flexibility < targetRotation && targetRotation < 180f)
+            targetRotation = flexibility;
+        else if (180f < targetRotation && targetRotation < 360f - flexibility)
+            targetRotation = 360f - flexibility;
+
+        // Executes the movement instructions on the top and bottom half of the character.
+        top.localRotation = Quaternion.Euler(0f, 0f, 360f- targetRotation);
+        bottom.localRotation = Quaternion.Euler(0f, 0f, targetRotation);
+    }
+
+    // Jump is currently broken, is not registering key presses
+    private void Jump() 
+    {
+        // Checks if the player is willing and able to jump
+        if (jumpIntent) 
+        {
+            // Checks if the player has jumped recently
+            if (jumpTimer >= jumpDelay)
+            {
+                // Determines the intended jump direction, and adds force either straight up or 45 degrees left/right.
+                float intentDirection = Input.GetAxis("Horizontal");
+
+                Vector2 direction = Vector2.up;
+                if (intentDirection != 0)
+                {
+                    direction = (((intentDirection > 0) ? -1 : 1) * Vector2.left + direction).normalized;
+                    rb.angularVelocity += horsepower * ((intentDirection > 0) ? -1 : 1);
+                }
+                rb.AddForce(direction * horsepower);
+
+                // Decrements relevant trackers on a successful jump attempt.
+                jumpTimer = 0;
+
+                //Plays jump audio file.
+                audio.Play(0);
+            }
+            jumpIntent = false;
+        }
+    }
+
+    // Disallows the player from moving
+    public void DisableMovement() 
+    {
+        movementEnabled = false;
+    }
+
+    // Allows the player to move
+    public void EnableMovement() 
+    {
+        movementEnabled = true;
+    }
+
+    public void Launch(Vector2 direction) 
+    {
+        rb.AddForce(direction);
+    }
+}
